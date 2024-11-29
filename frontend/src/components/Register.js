3adf83de68a273d512660e3ac15ea94d829a9095
Abani <55604768+AbaniKaurKohli@users.noreply.github.com>
2024-11-29 19:03:23 +0530

    Update Register.js
    Changed to access the /register

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -74,7 +74,7 @@ function Register() {
     };
 
     try {
-      const response = await fetch('http://localhost:8080/auth/register', {
+      const response = await fetch('http://localhost:8080/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
