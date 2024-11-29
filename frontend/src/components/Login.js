9cdb72a4318525f8ceff781e23cf9f364db31b63
Abani <55604768+AbaniKaurKohli@users.noreply.github.com>
2024-11-29 19:03:42 +0530

    Update Login.js
    chnaged to access the /login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -12,7 +12,7 @@ function Login({ setIsAuthenticated }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:8080/auth/login', {
+      const response = await fetch('http://localhost:8080/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
